834b321e4c06a10995686a44f20318f47fefe649
Tristan F. <LeoDog896@hotmail.com>
2023-11-28 12:59:14 +0000

    feat: base trpc server <-> client

--- apps/reciever/src/index.ts
+++ apps/reciever/src/index.ts
@@ -1,1 +1,10 @@
-console.log("test")
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import type { AppRouter } from "sender";
+
+const trpc = createTRPCProxyClient<AppRouter>({
+    links: [
+        httpBatchLink({
+            url: "http://localhost:3000"
+        })
+    ]
+});
--- apps/sender/src/index.ts
+++ apps/sender/src/index.ts
@@ -1,1 +1,12 @@
-console.log("test")
+import { createHTTPServer } from '@trpc/server/adapters/standalone'
+import { router } from "./trpc";
+
+const appRouter = router({
+
+});
+
+export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({ router: appRouter });
+
+server.listen(3000);
