c59af3d1f121f19a0ac5f226d0506da2c60ef10b
PIsForPotato <121689429+PIsForPotato@users.noreply.github.com>
2023-12-04 20:39:50 +0000

    feat: live data chart

--- apps/graph/src/main.ts
+++ apps/graph/src/main.ts
@@ -2,17 +2,31 @@ import "./app.css";
 import { Chart } from "chart.js/auto";
 
 const canvas = document.querySelector("canvas")!;
+let count = 0;
 
-const data = [12, 19, 3, 5, 2, 3];
-
-new Chart(canvas, {
+const chart = new Chart(canvas, {
   type: "line",
   data: {
-    labels: Array.from({ length: data.length }, (_, i) => i).map(String),
+    labels: [],
     datasets: [{
       label: "Depth",
-      data,
+      data: [],
       tension: 0.2
     }],
   },
 });
+
+function addData(chart: Chart<"line", number[], string>, label: string, newData: number) {
+  chart.data.labels?.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(newData);
+  });
+  chart.update();
+}
+
+window.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    addData(chart, count.toString(), parseInt(prompt("Enter the Number")!));
+    count++;
+  }
+})
