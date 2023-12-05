b85a73838c0c10588b364f2d8d2c244239b4fb81
Tristan F <LeoDog896@hotmail.com>
2023-12-05 08:38:58 -0500

    feat: onAdd subscriber

--- apps/sender/src/index.ts
+++ apps/sender/src/index.ts
@@ -1,13 +1,8 @@
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { observable } from "@trpc/server/observable";
+import { EventEmitter } from "events";
 import { z } from "zod";
 import { publicProcedure, router } from "./trpc.js";
-
-interface DataPoint {
-  timestamp: Date;
-  pressure: number;
-}
-
-const data: DataPoint[] = [];
 
 const packet = z.object({
   company: z.string(),
@@ -15,16 +10,28 @@ const packet = z.object({
   pressure: z.number(),
 });
 
+type Packet = z.infer<typeof packet>;
+
+const eventEmitter = new EventEmitter();
+const data: Packet[] = [];
+
 const appRouter = router({
   add: publicProcedure
     .input(packet)
     .mutation(({ input }) => {
-      data.push({
-        timestamp: new Date(),
-        pressure: input.pressure,
-      });
+      data.push({ ...input });
+      eventEmitter.emit("add", input);
       return data;
     }),
+  onAdd: publicProcedure.subscription(() => {
+    return observable<Packet>(emit => {
+      const onAdd = (data: Packet) => emit.next(data);
+
+      eventEmitter.on("add", onAdd);
+      return () => eventEmitter.off("add", onAdd);
+    });
+  }),
+  get: publicProcedure.query(() => data),
 });
 
 export type AppRouter = typeof appRouter;
