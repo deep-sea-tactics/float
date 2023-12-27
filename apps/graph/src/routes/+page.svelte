684e0585d96d656bce3a619c6766454a137a0394
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-26 21:34:10 -0500

    style: fmt

--- apps/graph/src/routes/+page.svelte
+++ apps/graph/src/routes/+page.svelte
@@ -7,69 +7,69 @@
 	let canvas: HTMLCanvasElement;
 	let count = 0;
 
-    function addData(chart: Chart<'line', number[], string>, label: string, newData: number) {
-        chart.data.labels?.push(label);
-        for (const dataset of chart.data.datasets) {
-            dataset.data.push(newData);
-        }
-        chart.update();
-    }
+	function addData(chart: Chart<'line', number[], string>, label: string, newData: number) {
+		chart.data.labels?.push(label);
+		for (const dataset of chart.data.datasets) {
+			dataset.data.push(newData);
+		}
+		chart.update();
+	}
 
-    onMount(() => {
-        const wsClient = createWSClient({
-            url: `ws://localhost:3000`
-        });
+	onMount(() => {
+		const wsClient = createWSClient({
+			url: `ws://localhost:3000`
+		});
 
-        const trpc = createTRPCProxyClient<AppRouter>({
-            links: [
-                wsLink({
-                    client: wsClient
-                })
-            ]
-        });
+		const trpc = createTRPCProxyClient<AppRouter>({
+			links: [
+				wsLink({
+					client: wsClient
+				})
+			]
+		});
 
-        const chart = new Chart(canvas, {
-            type: 'line',
-            data: {
-                labels: [],
-                datasets: [
-                    {
-                        label: 'Depth',
-                        data: [],
-                        tension: 0.2
-                    }
-                ]
-            }
-        });
+		const chart = new Chart(canvas, {
+			type: 'line',
+			data: {
+				labels: [],
+				datasets: [
+					{
+						label: 'Depth',
+						data: [],
+						tension: 0.2
+					}
+				]
+			}
+		});
 
-        trpc.onAdd.subscribe(undefined, {
-            onData: (data) => {
-                addData(chart, data.timestamp, data.pressure);
-                count++;
-            },
-            onError: (error) => {
-                console.error(error);
-            }
-        });
-    });
+		trpc.onAdd.subscribe(undefined, {
+			onData: (data) => {
+				addData(chart, data.timestamp, data.pressure);
+				count++;
+			},
+			onError: (error) => {
+				console.error(error);
+			}
+		});
+	});
 </script>
 
 <main>
-    <canvas bind:this={canvas} />
+	<canvas bind:this={canvas} />
 </main>
 
 <style>
-    canvas {
-        width: 100%;
-        height: 100%;
-    }
+	canvas {
+		width: 100%;
+		height: 100%;
+	}
 
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-    }
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
 </style>
