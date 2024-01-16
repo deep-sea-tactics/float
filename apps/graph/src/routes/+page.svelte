0d257c959a107b56e39875633586e5ac01cd4687
Tristan F. <LeoDog896@hotmail.com>
2024-01-16 13:40:37 +0000

    feat: base modal

--- apps/graph/src/routes/+page.svelte
+++ apps/graph/src/routes/+page.svelte
@@ -5,6 +5,8 @@
 	import type { AppRouter } from 'receiver';
 	import { onMount } from 'svelte';
 	import { transform } from 'cloud-url-resolver';
+	import { pushState } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	let canvas: HTMLCanvasElement;
 	let count = 0;
@@ -67,13 +69,55 @@
 			}
 		});
 	});
+
+	function showSettings() {
+		pushState('', {
+			showingModal: true
+		})
+	}
 </script>
 
 <main>
+	{#if $page.state.showingModal}
+		<button class="modal" on:click|self={() => history.back()}>
+			<div class="container">
+				<h1>Settings</h1>
+			</div>
+		</button>
+	{/if}
+
+	<button class="settings" on:click={showSettings}>Settings</button>
 	<canvas bind:this={canvas} />
 </main>
 
 <style>
+	button.settings {
+		top: 0;
+		left: 0;
+		margin: 1rem;
+		position: fixed;
+		padding: 0.5rem 1rem;
+		border: 2px solid black;
+	}
+
+	div.container {
+		background-color: white;
+		padding: 1rem;
+		border-radius: 1rem;
+		max-width: 50%;
+	}
+
+	button.modal {
+		position: fixed;
+		width: 100vw;
+		height: 100vh;
+		background-color: rgba(0, 0, 0, 0.5);
+		border: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
 	canvas {
 		width: 100%;
 		height: 100%;
