7a3e7a39e2e9be2dac147f57a96317afe3fa33c5
Tristan F. <LeoDog896@hotmail.com>
2024-01-16 13:48:09 +0000

    feat: data resetting

---
+++ apps/graph/src/lib/Chart.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import { createTRPCProxyClient, createWSClient, wsLink } from '@trpc/client';
+	// TODO: use chart.js tree-shaking
+	import { Chart } from 'chart.js/auto';
+	import type { AppRouter } from 'receiver';
+	import { onMount } from 'svelte';
+	import { transform } from 'cloud-url-resolver';
+
+	let canvas: HTMLCanvasElement;
+	let count = 0;
+	let startTS = Date.now();
+
+	function addData(chart: Chart<'line', number[], string>, label: string, newData: number) {
+		chart.data.labels?.push(label);
+		for (const dataset of chart.data.datasets) {
+			dataset.data.push(newData);
+		}
+		chart.update();
+	}
+
+	onMount(() => {
+		startTS = Date.now();
+		const wsClient = createWSClient({
+			url: transform(3000, 'ws')
+		});
+
+		const trpc = createTRPCProxyClient<AppRouter>({
+			links: [
+				wsLink({
+					client: wsClient
+				})
+			]
+		});
+
+		const chart = new Chart(canvas, {
+			type: 'line',
+			data: {
+				labels: [],
+				datasets: [
+					{
+						label: 'Depth',
+						data: [],
+						tension: 0.2
+					}
+				]
+			}
+		});
+
+		trpc.onAdd.subscribe(undefined, {
+			onData: (data) => {
+				const timestamp = data.timestamp - startTS;
+				// 5 mins, 5 seconds after running should be represented as 05:05
+				const minutes = Math.floor(timestamp / 1000 / 60);
+				const seconds = Math.floor((timestamp / 1000) % 60);
+
+				const timestampString = `${minutes.toString().padStart(2, '0')}:${seconds
+					.toString()
+					.padStart(2, '0')}`;
+
+				addData(chart, timestampString, data.pressure);
+
+				//addData(chart, data.timestamp, data.pressure);
+				count++;
+			},
+			onError: (error) => {
+				console.error(error);
+			}
+		});
+	});
+</script>
+
+<canvas bind:this={canvas} />
+
+<style>
+	canvas {
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- apps/graph/src/routes/+page.svelte
+++ apps/graph/src/routes/+page.svelte
@@ -1,74 +1,9 @@
 <script lang="ts">
-	import { createTRPCProxyClient, createWSClient, wsLink } from '@trpc/client';
-	// TODO: use chart.js tree-shaking
-	import { Chart } from 'chart.js/auto';
-	import type { AppRouter } from 'receiver';
-	import { onMount } from 'svelte';
-	import { transform } from 'cloud-url-resolver';
+	import Chart from "$lib/Chart.svelte";
 	import { pushState } from '$app/navigation';
 	import { page } from '$app/stores';
-
-	let canvas: HTMLCanvasElement;
-	let count = 0;
-	let startTS = Date.now();
-
-	function addData(chart: Chart<'line', number[], string>, label: string, newData: number) {
-		chart.data.labels?.push(label);
-		for (const dataset of chart.data.datasets) {
-			dataset.data.push(newData);
-		}
-		chart.update();
-	}
-
-	onMount(() => {
-		startTS = Date.now();
-		const wsClient = createWSClient({
-			url: transform(3000, 'ws')
-		});
-
-		const trpc = createTRPCProxyClient<AppRouter>({
-			links: [
-				wsLink({
-					client: wsClient
-				})
-			]
-		});
-
-		const chart = new Chart(canvas, {
-			type: 'line',
-			data: {
-				labels: [],
-				datasets: [
-					{
-						label: 'Depth',
-						data: [],
-						tension: 0.2
-					}
-				]
-			}
-		});
-
-		trpc.onAdd.subscribe(undefined, {
-			onData: (data) => {
-				const timestamp = data.timestamp - startTS;
-				// 5 mins, 5 seconds after running should be represented as 05:05
-				const minutes = Math.floor(timestamp / 1000 / 60);
-				const seconds = Math.floor((timestamp / 1000) % 60);
-
-				const timestampString = `${minutes.toString().padStart(2, '0')}:${seconds
-					.toString()
-					.padStart(2, '0')}`;
-
-				addData(chart, timestampString, data.pressure);
-
-				//addData(chart, data.timestamp, data.pressure);
-				count++;
-			},
-			onError: (error) => {
-				console.error(error);
-			}
-		});
-	});
+	
+	let chartSymbol = Symbol();
 
 	function showSettings() {
 		pushState('', {
@@ -82,12 +17,16 @@
 		<button class="modal" on:click|self={() => history.back()}>
 			<div class="container">
 				<h1>Settings</h1>
+				<button on:click={() => chartSymbol = Symbol()}>Reset All Data</button>
 			</div>
 		</button>
 	{/if}
 
 	<button class="settings" on:click={showSettings}>Settings</button>
-	<canvas bind:this={canvas} />
+	
+	{#key chartSymbol}
+		<Chart />
+	{/key}
 </main>
 
 <style>
@@ -98,6 +37,7 @@
 		position: fixed;
 		padding: 0.5rem 1rem;
 		border: 2px solid black;
+		z-index: 0;
 	}
 
 	div.container {
@@ -116,11 +56,7 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-	}
-
-	canvas {
-		width: 100%;
-		height: 100%;
+		z-index: 1;
 	}
 
 	main {
